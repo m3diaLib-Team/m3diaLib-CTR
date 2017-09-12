42cbc43f4f67cf194de81027624f7f40cfc646f5
Liam Minopulos <liam.minopulos@studierende.htl-donaustadt.at>
2017-09-12 09:28:08 +0200

    Extended applet functionality

--- m3dialib/includes/core/applet.hpp
+++ m3dialib/includes/core/applet.hpp
@@ -11,58 +11,58 @@ namespace m3d {
 	 * @brief Region values
 	 */
 	enum Region {
-		REGION_JPN = 0, ///< Japan
-		REGION_USA = 1, ///< USA
-		REGION_EUR = 2, ///< Europe
-		REGION_AUS = 3, ///< Australia
-		REGION_CHN = 4, ///< China
-		REGION_KOR = 5, ///< Korea
-		REGION_TWN = 6 	///< Taiwan
+		REGION_JPN, ///< Japan
+		REGION_USA, ///< USA
+		REGION_EUR, ///< Europe
+		REGION_AUS, ///< Australia
+		REGION_CHN, ///< China
+		REGION_KOR, ///< Korea
+		REGION_TWN 	///< Taiwan
 	};
 
 	/**
 	 * @brief Language values
 	 */
 	enum Language {
-		LANGUAGE_JP = 0, 	///< Japanese
-		LANGUAGE_EN = 1, 	///< English
-		LANGUAGE_FR = 2, 	///< French
-		LANGUAGE_DE = 3, 	///< German
-		LANGUAGE_IT = 4, 	///< Italian
-		LANGUAGE_ES = 5, 	///< Spanish
-		LANGUAGE_ZH = 6, 	///< Simplified chinese
-		LANGUAGE_KO = 7, 	///< Korean
-		LANGUAGE_NL = 8, 	///< Dutch
-		LANGUAGE_PT = 9, 	///< Portugese
-		LANGUAGE_RU = 10, ///< Russian
-		LANGUAGE_TW = 11 	///< Traditional chinese
+		LANGUAGE_JP, 	///< Japanese
+		LANGUAGE_EN, 	///< English
+		LANGUAGE_FR, 	///< French
+		LANGUAGE_DE, 	///< German
+		LANGUAGE_IT, 	///< Italian
+		LANGUAGE_ES, 	///< Spanish
+		LANGUAGE_ZH, 	///< Simplified chinese
+		LANGUAGE_KO, 	///< Korean
+		LANGUAGE_NL, 	///< Dutch
+		LANGUAGE_PT, 	///< Portugese
+		LANGUAGE_RU,	///< Russian
+		LANGUAGE_TW 	///< Traditional chinese
 	};
 
 	/**
 	 * @brief Defines ids of LibApps
 	 */
 	enum LibAppId {
-		APPID_NONE 								= 0,
-		APPID_HOMEMENU 						= 0x101, ///< Home menu
-		APPID_CAMERA 							= 0x110, ///< Camera applet
-		APPID_FRIENDS_LIST 				= 0x112, ///< Friends list applet
-		APPID_GAME_NOTES 					= 0x113, ///< Game notes applet
-		APPID_WEB 								= 0x114, ///< Internet browser
-		APPID_INSTRUCTION_MANUAL 	= 0x115, ///< Instruction manual applet
-		APPID_NOTIFICATIONS 			= 0x116, ///< Notifications applet
-		APPID_MIIVERSE 						= 0x117, ///< Miiverse applet
-		APPID_MIIVERSE_POSTING 		= 0x118, ///< Miiverse posting applet
-		APPID_AMIIBO_SETTINGS 		= 0x119, ///< Amiibo settings applet
-		APPID_APPLICATION 				= 0x300, ///< Application (?)
-		APPID_ESHOP 							= 0x301, ///< eShop (tiger)
-		APPID_SOFTWARE_KEYBOARD 	= 0x401, ///< Software keyboard (do not use, use the m3d::SoftwareKeyboard class instead)
-		APPID_APPLETED 						= 0x402, ///< AppletEd (Mii editor)
-		APPID_PNOTE_AP 						= 0x404, ///< Photo selector
-		APPID_SNOTE_AP 						= 0x405, ///< Sound selector
-		APPID_ERROR 							= 0x406, ///< Error (do not use, use the m3d::Error class instead)
-		APPID_MINT 								= 0x407, ///< Mint (eShop)
-		APPID_EXTRAPAD 						= 0x408, ///< Extrapad
-		APPID_MEMOLIB 						= 0x409  ///< Memolib
+		LIBAPP_NONE,
+		LIBAPP_HOMEMENU, 						///< Home menu
+		LIBAPP_CAMERA, 							///< Camera applet
+		LIBAPP_FRIENDS_LIST, 				///< Friends list applet
+		LIBAPP_GAME_NOTES, 					///< Game notes applet
+		LIBAPP_WEB, 								///< Internet browser
+		LIBAPP_INSTRUCTION_MANUAL, 	///< Instruction manual applet
+		LIBAPP_NOTIFICATIONS, 			///< Notifications applet
+		LIBAPP_MIIVERSE, 						///< Miiverse applet
+		LIBAPP_MIIVERSE_POSTING, 		///< Miiverse posting applet
+		LIBAPP_AMIIBO_SETTINGS, 		///< Amiibo settings applet
+		LIBAPP_APPLICATION, 				///< Application (?)
+		LIBAPP_ESHOP, 							///< eShop (tiger)
+		LIBAPP_SOFTWARE_KEYBOARD, 	///< Software keyboard (do not use, use the m3d::SoftwareKeyboard class instead)
+		LIBAPP_APPLETED, 						///< AppletEd (Mii editor)
+		LIBAPP_PNOTE_AP, 						///< Photo selector
+		LIBAPP_SNOTE_AP, 						///< Sound selector
+		LIBAPP_ERROR, 							///< Error (do not use, use the m3d::Error class instead)
+		LIBAPP_MINT, 								///< Mint (eShop)
+		LIBAPP_EXTRAPAD, 						///< Extrapad
+		LIBAPP_MEMOLIB 							///< Memolib
 	};
 
 	/**
@@ -76,6 +76,11 @@ namespace m3d {
 		Applet();
 
 		/**
+		 * @brief Destructs the applet
+		 */
+		~Applet();
+
+		/**
 		 * @brief Returns if the applet is still running
 		 */
 		bool isRunning();
@@ -84,6 +89,11 @@ namespace m3d {
 		 * @brief Exits the applet
 		 */
 		void exit();
+
+		/**
+		 * @brief Reboots the system
+		 */
+		void reboot();
 
 		/**
 		 * @brief Sets whether stand-by mode is allowed
@@ -104,7 +114,7 @@ namespace m3d {
 		 *
 		 * To get the id of the app you want to launch, please refer to <a href="http://3dbrew.org/wiki/Title_list#00040010_-_System_Applications" target="_blank">http://3dbrew.org/wiki/Title_list#00040010_-_System_Applications</a>.
 		 */
-		bool launchSystemApp(u64 t_appId);
+		void launchSystemApp(u64 t_appId);
 
 		/**
 		 * @brief Launches a library app (extrapad, memolib, ...)
@@ -112,6 +122,12 @@ namespace m3d {
 		 * @return      The success state of the launch
 		 */
 		bool launchLibApp(m3d::LibAppId t_id);
+
+		/**
+		 * Returns whether the system is a New3DS
+		 * @return Whether the system is a New3DS or not
+		 */
+		bool getNew3ds();
 
 	private:
 		/* data */
--- m3dialib/source/core/applet.cpp
+++ m3dialib/source/core/applet.cpp
@@ -1,9 +1,18 @@
 #include <3ds.h>
+#include <cstring>
 #include "core/applet.hpp"
 
 namespace m3d {
 	Applet::Applet() {
-		// Do nothing
+		aptInit();
+		cfguInit();
+		ptmuInit();
+	}
+
+	Applet::~Applet() {
+		ptmuExit();
+		cfguExit();
+		aptExit();
 	}
 
 	bool Applet::isRunning() {
@@ -11,7 +20,113 @@ namespace m3d {
 	}
 
 	void Applet::exit() {
-		// aptSendCommand(APTCMD_EXIT);
-		APT_CloseApplication(0, 0, 0);
+		aptExit();
+		APT_CloseApplication(nullptr, 0, 0);
+	}
+
+	void Applet::reboot() {
+		APT_HardwareResetAsync();
+	}
+
+	void Applet::setSleepAllowed(bool t_allowed) {
+		aptSetSleepAllowed(t_allowed);
+	}
+
+	bool Applet::getSleepAllowed() {
+		return aptIsSleepAllowed();
+	}
+
+	void Applet::launchSystemApp(u64 t_appId) {
+		u8 param[0x300];
+		u8 hmac[0x20];
+
+		memset(param, 0, sizeof(param));
+		memset(hmac, 0, sizeof(hmac));
+
+		APT_PrepareToDoApplicationJump(0, t_appId, 0);
+		APT_DoApplicationJump(param, sizeof(param), hmac);
+	}
+
+	bool Applet::launchLibApp(m3d::LibAppId t_id) {
+		NS_APPID id;
+
+		switch(t_id) {
+			case LIBAPP_HOMEMENU:
+				id = APPID_HOMEMENU;
+				break;
+			case LIBAPP_CAMERA:
+				id = APPID_CAMERA;
+				break;
+			case LIBAPP_FRIENDS_LIST:
+				id = APPID_FRIENDS_LIST;
+				break;
+			case LIBAPP_GAME_NOTES:
+				id = APPID_GAME_NOTES;
+				break;
+			case LIBAPP_WEB:
+				id = APPID_WEB;
+				break;
+			case LIBAPP_INSTRUCTION_MANUAL:
+				id = APPID_INSTRUCTION_MANUAL;
+				break;
+			case LIBAPP_NOTIFICATIONS:
+				id = APPID_NOTIFICATIONS;
+				break;
+			case LIBAPP_MIIVERSE:
+				id = APPID_MIIVERSE;
+				break;
+			case LIBAPP_MIIVERSE_POSTING:
+				id = APPID_MIIVERSE_POSTING;
+				break;
+			case LIBAPP_AMIIBO_SETTINGS:
+				id = APPID_AMIIBO_SETTINGS;
+				break;
+			case LIBAPP_APPLICATION:
+				id = APPID_APPLICATION;
+				break;
+			case LIBAPP_ESHOP:
+				id = APPID_ESHOP;
+				break;
+			case LIBAPP_SOFTWARE_KEYBOARD:
+				id = APPID_SOFTWARE_KEYBOARD;
+				break;
+			case LIBAPP_APPLETED:
+				id = APPID_APPLETED;
+				break;
+			case LIBAPP_PNOTE_AP:
+				id = APPID_PNOTE_AP;
+				break;
+			case LIBAPP_SNOTE_AP:
+				id = APPID_SNOTE_AP;
+				break;
+			case LIBAPP_ERROR:
+				id = APPID_ERROR;
+				break;
+			case LIBAPP_MINT:
+				id = APPID_MINT;
+				break;
+			case LIBAPP_EXTRAPAD:
+				id = APPID_EXTRAPAD;
+				break;
+			case LIBAPP_MEMOLIB:
+				id = APPID_MEMOLIB;
+				break;
+			default:
+				id = APPID_NONE;
+		}
+
+		u32 aptbuf[0x400/4];
+
+		memset(aptbuf, 0, sizeof(aptbuf));
+		if (!aptLaunchLibraryApplet(id, aptbuf, sizeof(aptbuf), 0))
+			return false;
+
+		return true;
+	}
+
+	bool Applet::getNew3ds() {
+		bool state;
+		APT_CheckNew3DS(&state);
+		return state;
 	}
 } /* m3d */
