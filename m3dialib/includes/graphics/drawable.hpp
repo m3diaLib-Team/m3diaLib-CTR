a353ebb8c7b9317c8cbbc05dfd9edc917d563aee
Liam Minopulos <liam.minopulos@studierende.htl-donaustadt.at>
2017-09-16 00:29:36 +0200

    Added base class for drawables

---
+++ m3dialib/includes/graphics/drawable.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file drawable.hpp
+ * @brief Defines the base class for all drawable classes
+ */
+#pragma once
+#include <3ds.h>
+#include <citro3d.h>
+
+namespace m3d {
+	/**
+	 * @brief The base class for all drawable classes.
+	 *
+	 * To create your own drawable class, create a child class of this one and implement the draw()-function.
+	 */
+	class Drawable {
+	public:
+		/**
+		 * @brief Draws the drawable object
+		 * @param t_side The current 3D side (0 = left, 1 = right)
+		 *
+		 * Implement this function in your own class to draw stuff on the screen using Citro3D-functions.
+		 */
+		virtual void draw(int t_side) = 0;
+	};
+} /* m3d */
