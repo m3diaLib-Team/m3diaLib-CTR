958a415d35c81c8c40f56f3062699c1ed5e4313b
Liam Minopulos <stunthacks@gmail.com>
2018-08-26 23:14:36 +0200

    Updated parameters and removed useless enum

--- m3dialib/includes/graphics/drawable.hpp
+++ m3dialib/includes/graphics/drawable.hpp
@@ -8,19 +8,6 @@
 
 namespace m3d {
     /**
-     * @brief Defines the different interpolation modes
-     */
-    enum InterpolationMode {
-        GPU_REPLACE      = 0x00, ///< Replace
-        GPU_MODULATE     = 0x01, ///< Modulate
-        GPU_ADD          = 0x02, ///< Add
-        GPU_ADD_SIGNED   = 0x03, ///< Signed add
-        GPU_INTERPOLATE  = 0x04, ///< Interpolate (default)
-        GPU_MULTIPLY_ADD = 0x08, ///< Multiply, then add
-        GPU_ADD_MULTIPLY = 0x09, ///< Add, then multiply
-    };
-
-    /**
      * @brief The base class for all drawable classes.
      *
      * To create your own drawable class, create a child class of this one and implement the draw()-function.
@@ -30,12 +17,9 @@ namespace m3d {
         /**
          * @brief Draws the drawable object
          * @param t_side The current 3D side (0 = left, 1 = right)
-         * @param t_projection The projection uniform of the shader
-         * @param t_transform The transform uniform of the shader
-         * @param t_useTransform The useTransform uniform of the shader
          *
-         * Implement this function in your own class to draw stuff on the screen using Citro3D-functions.
+         * Implement this function in your own class to draw stuff on the screen using Citro2D-functions.
          */
-        virtual void draw(int t_side, int t_projection, int t_transform, int t_useTransform) = 0;
+        virtual void draw(int t_side) = 0;
     };
 } /* m3d */
