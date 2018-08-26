626655d6efa2443af7d70f694bad58a2243ebf5b
Liam Minopulos <stunthacks@gmail.com>
2018-08-26 16:19:52 +0200

    Rewrote Shape-class to use Citro2D

--- m3dialib/source/graphics/shape.cpp
+++ m3dialib/source/graphics/shape.cpp
@@ -1,90 +1,57 @@
-#include "graphics/drawables/shape.hpp"
 #include "graphics/earcut.hpp"
+#include "graphics/drawables/shape.hpp"
+#include <citro2d.h>
 
 namespace m3d {
     Shape::Shape() :
-        m_changed(true),
-        m_interpolationMode(m3d::GPU_INTERPOLATE) { /* do nothing */ }
+            m_changed(false) { /* do nothing */ }
 
-    Shape::~Shape() {
-        linearFree(m_internalVertices);
-    }
-
-    void Shape::addVertex(m3d::ColoredVertex t_vertex) {
-        m_vertices.push_back(t_vertex);
+    void Shape::addVertex(int t_x, int t_y, m3d::Color t_color) {
+        m_vertices.push_back(std::make_pair((m3d::Vector2f) { t_x, t_y }, t_color));
         m_changed = true;
     }
 
-    void Shape::addVertex(int t_x, int t_y, int t_z, m3d::Color t_color) {
-        m3d::ColoredVertex vertex = { { (float) t_x, (float) t_y, (float) t_z }, t_color };
-        m_vertices.push_back(vertex);
+    void Shape::addVertex(m3d::Vector2f t_vertex, m3d::Color t_color) {
+        m_vertices.push_back(std::make_pair((m3d::Vector2f) { t_vertex.u, t_vertex.v }, t_color));
         m_changed = true;
     }
 
     void Shape::clearVertices() {
         m_vertices.clear();
-        linearFree(m_internalVertices);
         m_changed = true;
     }
 
-    void Shape::setInterpolationMode(m3d::InterpolationMode t_interpolationMode) {
-        m_interpolationMode = t_interpolationMode;
-    }
-
-    m3d::InterpolationMode Shape::getInterpolationMode() {
-        return m_interpolationMode;
-    }
-
-    void Shape::draw(int, int, int, int) {
+    void Shape::draw(int) {
         if (m_changed) {
             m_changed = false;
-            linearFree(m_internalVertices);
-            linearFree(m_elementData);
-            m_internalVertices = static_cast<m3d::InternalColoredVertex*>(linearAlloc(m_vertices.size() * sizeof(m3d::InternalColoredVertex)));
-
-            if (m_internalVertices == nullptr) return;
 
             std::vector<std::vector<std::array<double, 2>>> polygon;
             std::vector<std::array<double, 2>> polyline;
 
-            for (unsigned int i = 0; i < m_vertices.size(); i++) {
-                float x = m_vertices[i].position.x,
-                      y = m_vertices[i].position.y,
-                    red = (float) m_vertices[i].color.getRed() / 255,
-                  green = (float) m_vertices[i].color.getGreen() / 255,
-                   blue = (float) m_vertices[i].color.getBlue() / 255,
-                  alpha = (float) m_vertices[i].color.getAlpha() / 255;
-
-                m_internalVertices[i] = (m3d::InternalColoredVertex) { {x, y, 0.5f}, {red, green, blue, alpha} };
-                polyline.push_back({ x, y });
+            for (const auto &vertex : m_vertices) {
+                polyline.push_back({ vertex.first.u, vertex.first.v });
             }
 
             polygon.push_back(polyline);
             m_indices = mapbox::earcut<uint16_t>(polygon);
-
-            m_elementData = static_cast<s16*>(linearAlloc(m_indices.size() * sizeof(s16)));
-
-            for (unsigned int i = 0; i < m_indices.size(); i++) {
-                m_elementData[i] = m_indices[i];
-            }
         }
 
-        // there be dragons
-        C3D_AttrInfo* attrInfo = C3D_GetAttrInfo();
-        AttrInfo_Init(attrInfo);
-        AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 3); // position
-        AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 4); // color
+        for (int i = 0; i < m_indices.size(); i += 3) {
+            C2D_DrawTriangle(
+                        m_vertices[m_indices[i]].first.u,
+                        m_vertices[m_indices[i]].first.v,
+                        m_vertices[m_indices[i]].second.getRgba8(),
 
-        C3D_BufInfo* bufInfo = C3D_GetBufInfo();
-        BufInfo_Init(bufInfo);
-        BufInfo_Add(bufInfo, m_internalVertices, sizeof(m3d::InternalColoredVertex), 2, 0x10);
+                        m_vertices[m_indices[i + 1]].first.u,
+                        m_vertices[m_indices[i + 1]].first.v,
+                        m_vertices[m_indices[i + 1]].second.getRgba8(),
 
-        C3D_TexEnv* env = C3D_GetTexEnv(0);
-        C3D_TexEnvSrc(env, C3D_Both, GPU_PRIMARY_COLOR, 0, 0);
-        C3D_TexEnvOp(env, C3D_Both, 0, 0, 0);
-        C3D_TexEnvFunc(env, C3D_Both, m_interpolationMode);
+                        m_vertices[m_indices[i + 2]].first.u,
+                        m_vertices[m_indices[i + 2]].first.v,
+                        m_vertices[m_indices[i + 2]].second.getRgba8(),
 
-        // Draw the VBO
-        C3D_DrawElements(GPU_TRIANGLES, m_indices.size(), GPU_UNSIGNED_BYTE, m_elementData);
+                        0.5f
+                    );
+        }
     }
 } /* m3d */
