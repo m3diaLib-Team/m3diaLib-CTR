5506404b39e9dd575a97bc92e0571b19e6aacdbc
Liam Minopulos <stunthacks@gmail.com>
2018-09-06 12:01:23 +0200

    Add breakpoint macros

--- m3dialib/includes/m3dia.hpp
+++ m3dialib/includes/m3dia.hpp
@@ -13,3 +13,24 @@
 #include "./graphics/graphics.hpp"
 #include "./input/input.hpp"
 #include "./utils/utils.hpp"
+
+#ifdef M3D_DEBUG
+/**
+ * @brief Breaks the execution and waits for the A-button to be pressed
+ *
+ * To activate, define M3D_DEBUG before including this file
+ */
+#define M3D_BREAKPOINT() \
+        std::cout << "\u001b[33;1mBreakpoint reached: \n" << (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) << ":" << __LINE__ << "\u001b[0m\n"; \
+        while (true) { hidScanInput(); if (m3d::buttons::buttonPressed(m3d::buttons::BUTTON_A)) break; }
+
+/**
+ * @brief Breaks the execution and waits for the A-button to be pressed
+ * @param  msg A message to display
+ *
+ * To activate, define M3D_DEBUG before including this file
+ */
+#define M3D_DEBUGPOINT(msg) \
+    std::cout << "\u001b[33;1mBreakpoint reached: \n" << (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) << ":" << __LINE__ << ": " << msg << "\u001b[0m\n"; \
+    while (true) { hidScanInput(); if (m3d::buttons::buttonPressed(m3d::buttons::BUTTON_A)) break; }
+#endif
