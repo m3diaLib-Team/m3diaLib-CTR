4c23f65e4e60d955bc1b4e56696e54705f202f39
Liam Minopulos <stunthacks@gmail.com>
2018-09-20 16:13:15 +0200

    Add 3D renderqueue

--- m3dialib/includes/m3d/graphics/screen.hpp
+++ m3dialib/includes/m3d/graphics/screen.hpp
@@ -6,10 +6,10 @@
 #define SCREEN_H
 
 #pragma once
-#include <vector>
-#include <map>
-#include <cstring>
 #include <citro2d.h>
+#include <cstring>
+#include <map>
+#include <vector>
 #include "renderTarget.hpp"
 #include "drawable.hpp"
 #include "color.hpp"
@@ -23,22 +23,6 @@ namespace m3d {
      */
     class Screen {
     public:
-        /**
-         * @brief Defines the screen targets (top/bottom)
-         */
-        enum ScreenTarget {
-            Top,     ///< Top screen
-            Bottom   ///< Bottom/touch screen
-        };
-
-        /**
-         * @brief Defines the two sides in stereoscopic 3D mode
-         */
-        enum Stereo3dSide {
-            Left  = 0, ///< Left side
-            Right = 1  ///< Right side
-        };
-
         /**
          * @brief Initializes the m3d::Screen object
          * @param t_enable3d Whether to enable stereoscopic 3D by default
@@ -67,28 +51,30 @@ namespace m3d {
          * @param t_color  The color to clear the screen with
          * @param t_target The screen to clear set the color for
          */
-        void setClearColor(m3d::Color t_color, m3d::Screen::ScreenTarget t_target);
+        void setClearColor(m3d::Color t_color, m3d::RenderContext::ScreenTarget t_target);
 
         /**
          * @brief Returns the clear color for the given screen
          * @param  t_target The screen to get the color from
          * @return          The set clear color for the screen
          */
-        m3d::Color getClearColor(m3d::Screen::ScreenTarget t_target);
+        m3d::Color getClearColor(m3d::RenderContext::ScreenTarget t_target);
 
         /**
          * @brief Draws something on the top screen
          * @param t_object The object that should be drawn
+         * @param t_mode   The drawing-mode
          * @param t_layer The z-inex the object should be drawn at
          */
-        void drawTop(m3d::Drawable& t_object, int t_layer = 0);
+        void drawTop(m3d::Drawable& t_object, m3d::RenderContext::Mode t_mode = m3d::RenderContext::Mode::Flat, int t_layer = 0);
 
         /**
          * @brief Draws something on the bottom screen
          * @param t_object The object that should be drawn
+         * @param t_mode   The drawing-mode
          * @param t_layer The z-inex the object should be drawn at
          */
-        void drawBottom(m3d::Drawable& t_object, int t_layer = 0);
+        void drawBottom(m3d::Drawable& t_object, m3d::RenderContext::Mode t_mode = m3d::RenderContext::Mode::Flat, int t_layer = 0);
 
         /**
          * @brief Renders the current screen
@@ -101,7 +87,7 @@ namespace m3d {
          * @param t_target The target screen
          * @return The width of the screen in pixels
          */
-        static int getScreenWidth(m3d::Screen::ScreenTarget t_target);
+        static int getScreenWidth(m3d::RenderContext::ScreenTarget t_target);
 
         /**
          * @brief Returns the height of the screen
@@ -109,18 +95,23 @@ namespace m3d {
          */
         static int getScreenHeight();
 
-    private:
+        /**
+         * @brief Clears the screen manually
+         */
         void clear();
+    private:
+        void prepare();
 
         /* data */
-        int m_projection, m_transform, m_useTransform;
+        int m_projectionUniform, m_modelViewUniform, m_lightVecUniform, m_lightHalfVecUniform, m_lightColorUniform, m_materialUniform;
         bool m_3dEnabled;
         m3d::Color m_clearColorTop, m_clearColorBottom;
         m3d::RenderTarget *m_targetTopLeft, *m_targetTopRight, *m_targetBottom;
-        std::map<int, std::vector<m3d::Drawable*>, std::less<int>> m_drawStackTop, m_drawStackBottom;
+        std::map<int, std::vector<m3d::Drawable*>, std::less<int>> m_drawStackTop2d, m_drawStackTop3d, m_drawStackBottom2d, m_drawStackBottom3d;
         DVLB_s *m_dvlb;
         shaderProgram_s m_shader;
-
+        C3D_AttrInfo* m_attributeInfo;
+        C3D_Mtx m_projection, m_projectionBottom, m_modelView, m_modelViewBottom;
     };
 } /* m3d */
 
--- m3dialib/source/graphics/screen.cpp
+++ m3dialib/source/graphics/screen.cpp
@@ -1,6 +1,8 @@
 #include <citro2d.h>
 #include "m3d/graphics/screen.hpp"
 #include "m3d/graphics/color.hpp"
+#include "m3d/private/graphics.hpp"
+#include "render3d_shbin.h"
 
 namespace m3d {
     Screen::Screen(bool t_enable3d) :
@@ -9,7 +11,6 @@ namespace m3d {
         gfxInitDefault();
         C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
         C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
-        C2D_Prepare();
         gfxSet3D(t_enable3d);
         C3D_DepthTest(true, GPU_ALWAYS, GPU_WRITE_ALL);
         m_3dEnabled = t_enable3d;
@@ -17,10 +18,30 @@ namespace m3d {
         m_targetTopRight = new m3d::RenderTarget(400, 240, GFX_TOP, GFX_RIGHT);
         m_targetBottom   = new m3d::RenderTarget(320, 240, GFX_BOTTOM, GFX_LEFT);
 
+        u32 flags = GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO);
+
+        C3D_RenderTargetSetOutput(m_targetTopLeft->getRenderTarget(),  GFX_TOP,    GFX_LEFT,  flags);
+        C3D_RenderTargetSetOutput(m_targetTopRight->getRenderTarget(), GFX_TOP,    GFX_RIGHT, flags);
+        C3D_RenderTargetSetOutput(m_targetBottom->getRenderTarget(),   GFX_BOTTOM, GFX_LEFT,  flags);
+
+        m_dvlb = DVLB_ParseFile((u32*) render3d_shbin, render3d_shbin_size);
+        shaderProgramInit(&m_shader);
+        shaderProgramSetVsh(&m_shader, &m_dvlb->DVLE[0]);
+
+        // get location of uniforms used in the vertex shader.
+        m_projectionUniform = shaderInstanceGetUniformLocation(m_shader.vertexShader, "projection");
+        m_modelViewUniform = shaderInstanceGetUniformLocation(m_shader.vertexShader, "modelView");
+        m_lightVecUniform     = shaderInstanceGetUniformLocation(m_shader.vertexShader, "lightVec");
+        m_lightHalfVecUniform = shaderInstanceGetUniformLocation(m_shader.vertexShader, "lightHalfVec");
+        m_lightColorUniform     = shaderInstanceGetUniformLocation(m_shader.vertexShader, "lightClr");
+        m_materialUniform     = shaderInstanceGetUniformLocation(m_shader.vertexShader, "material");
+
         clear();
     }
 
     Screen::~Screen() {
+        shaderProgramFree(&m_shader);
+        DVLB_Free(m_dvlb);
         C2D_Fini();
         C3D_Fini();
         gfxExit();
@@ -36,95 +57,260 @@ namespace m3d {
         m_clearColorBottom = t_color;
     }
 
-    void Screen::setClearColor(m3d::Color t_color, m3d::Screen::ScreenTarget t_target) {
-        t_target == m3d::Screen::ScreenTarget::Top ?
+    void Screen::setClearColor(m3d::Color t_color, m3d::RenderContext::ScreenTarget t_target) {
+        t_target == m3d::RenderContext::ScreenTarget::Top ?
                     m_clearColorTop = t_color :
                     m_clearColorBottom = t_color;
     }
 
-    m3d::Color Screen::getClearColor(m3d::Screen::ScreenTarget t_target) {
-        return t_target == m3d::Screen::ScreenTarget::Top ? m_clearColorTop : m_clearColorBottom;
+    m3d::Color Screen::getClearColor(m3d::RenderContext::ScreenTarget t_target) {
+        return t_target == m3d::RenderContext::ScreenTarget::Top ? m_clearColorTop : m_clearColorBottom;
     }
 
-    void Screen::drawTop(m3d::Drawable& t_object, int t_layer) {
-        if(m_drawStackTop.count(t_layer) > 0) {
-            m_drawStackTop[t_layer].insert(m_drawStackTop[t_layer].end(), &t_object);
+    void Screen::drawTop(m3d::Drawable& t_object, m3d::RenderContext::Mode t_mode, int t_layer) {
+        if (t_mode == m3d::RenderContext::Mode::Flat) {
+            if(m_drawStackTop2d.count(t_layer) > 0) {
+                m_drawStackTop2d[t_layer].insert(m_drawStackTop2d[t_layer].end(), &t_object);
+            } else {
+                std::vector<m3d::Drawable*> newStack;
+                newStack.push_back(&t_object);
+                m_drawStackTop2d.insert(std::make_pair(t_layer, newStack));
+            }
         } else {
-            std::vector<m3d::Drawable*> newStack;
-            newStack.push_back(&t_object);
-            m_drawStackTop.insert(std::make_pair(t_layer, newStack));
+            if(m_drawStackTop3d.count(t_layer) > 0) {
+                m_drawStackTop3d[t_layer].insert(m_drawStackTop3d[t_layer].end(), &t_object);
+            } else {
+                std::vector<m3d::Drawable*> newStack;
+                newStack.push_back(&t_object);
+                m_drawStackTop3d.insert(std::make_pair(t_layer, newStack));
+            }
         }
     }
 
-    void Screen::drawBottom(m3d::Drawable& t_object, int t_layer) {
-        if(m_drawStackBottom.count(t_layer) > 0) {
-            m_drawStackBottom[t_layer].insert(m_drawStackBottom[t_layer].end(), &t_object);
+    void Screen::drawBottom(m3d::Drawable& t_object, m3d::RenderContext::Mode t_mode, int t_layer) {
+        if (t_mode == m3d::RenderContext::Mode::Flat) {
+            if(m_drawStackBottom2d.count(t_layer) > 0) {
+                m_drawStackBottom2d[t_layer].insert(m_drawStackBottom2d[t_layer].end(), &t_object);
+            } else {
+                std::vector<m3d::Drawable*> newStack;
+                newStack.push_back(&t_object);
+                m_drawStackBottom2d.insert(std::make_pair(t_layer, newStack));
+            }
         } else {
-            std::vector<m3d::Drawable*> newStack;
-            newStack.push_back(&t_object);
-            m_drawStackBottom.insert(std::make_pair(t_layer, newStack));
+            if(m_drawStackBottom3d.count(t_layer) > 0) {
+                m_drawStackBottom3d[t_layer].insert(m_drawStackBottom3d [t_layer].end(), &t_object);
+            } else {
+                std::vector<m3d::Drawable*> newStack;
+                newStack.push_back(&t_object);
+                m_drawStackBottom3d.insert(std::make_pair(t_layer, newStack));
+            }
         }
     }
 
     void Screen::render(bool t_clear) {
-        if(m_drawStackTop.size() > 0 || m_drawStackBottom.size() > 0) {
-            C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 
-            if (t_clear) {
-                m_targetTopLeft->clear();
-                m_targetTopRight->clear();
-                m_targetBottom->clear();
-                clear();
-            }
+        if (t_clear) {
+            m_targetTopLeft->clear();
+            m_targetTopRight->clear();
+            m_targetBottom->clear();
+            clear();
+        }
 
-            if(m_drawStackBottom.size() > 0) {
-                C2D_SceneBegin(m_targetBottom->getRenderTarget());
+        // draw 3d
+        if(m_drawStackTop3d.size() > 0 || m_drawStackBottom3d.size() > 0) {
+            prepare();
 
-                for(const auto &entry : m_drawStackBottom) { // for every layer
+            if(m_drawStackBottom3d.size() > 0) {
+                C3D_FrameDrawOn(m_targetBottom->getRenderTarget());
+
+                for(const auto &entry : m_drawStackBottom3d) { // for every layer
                     for(const auto &drawable : entry.second) { // draw every object
-                        drawable->draw(m_3dEnabled, m3d::Screen::Stereo3dSide::Left);
+                        drawable->draw(m3d::RenderContext(
+                                m_projectionUniform,   // projectionUniform
+                                m_modelViewUniform,    // modelViewUniform
+                                m_lightVecUniform,     // lightVecUniform
+                                m_lightHalfVecUniform, // lightHalfVecUniform
+                                m_lightColorUniform,   // lightColorUniform
+                                m_materialUniform,     // materialUniform
+                                m_3dEnabled, // 3dEnabled
+                                m3d::RenderContext::Mode::Spatial,        // mode
+                                m3d::RenderContext::Stereo3dSide::Left,   // side
+                                m3d::RenderContext::ScreenTarget::Bottom, // target
+                                m_projection, // projection
+                                m_modelView   // model
+                            ));
                     }
                 }
+
+                m_drawStackBottom3d.clear();
             }
 
-            if(m_drawStackTop.size() > 0) {
+            if (m_drawStackTop3d.size() > 0) {
+                C3D_FrameDrawOn(m_targetTopLeft->getRenderTarget());
+
+                for (const auto &entry : m_drawStackTop3d) { // for every layer
+                    for (const auto &drawable : entry.second) { // draw every object
+                        drawable->draw(m3d::RenderContext(
+                                m_projectionUniform,   // projectionUniform
+                                m_modelViewUniform,    // modelViewUniform
+                                m_lightVecUniform,     // lightVecUniform
+                                m_lightHalfVecUniform, // lightHalfVecUniform
+                                m_lightColorUniform,   // lightColorUniform
+                                m_materialUniform,     // materialUniform
+                                m_3dEnabled, // 3dEnabled
+                                m3d::RenderContext::Mode::Spatial,      // mode
+                                m3d::RenderContext::Stereo3dSide::Left, // side
+                                m3d::RenderContext::ScreenTarget::Top,  // target
+                                m_projection, // projection
+                                m_modelView   // model
+                            ));
+                    }
+                }
+
+                if (m_3dEnabled) {
+                    C3D_FrameDrawOn(m_targetTopRight->getRenderTarget());
+
+                    for (const auto &entry : m_drawStackTop3d) { // for every layer
+                        for (const auto &drawable : entry.second) { // draw every object
+                            drawable->draw(m3d::RenderContext(
+                                    m_projectionUniform,   // projectionUniform
+                                    m_modelViewUniform,    // modelViewUniform
+                                    m_lightVecUniform,     // lightVecUniform
+                                    m_lightHalfVecUniform, // lightHalfVecUniform
+                                    m_lightColorUniform,   // lightColorUniform
+                                    m_materialUniform,     // materialUniform
+                                    m_3dEnabled, // 3dEnabled
+                                    m3d::RenderContext::Mode::Spatial,       // mode
+                                    m3d::RenderContext::Stereo3dSide::Right, // side
+                                    m3d::RenderContext::ScreenTarget::Top,   // target
+                                    m_projection, // projection
+                                    m_modelView   // modelview
+                                ));
+                        }
+                    }
+                }
+
+                m_drawStackTop3d.clear();
+            }
+        }
+
+        // draw 2d
+        if (m_drawStackTop2d.size() > 0 || m_drawStackBottom2d.size() > 0) {
+            C2D_Prepare();
+
+            if(m_drawStackBottom2d.size() > 0) {
+                C2D_SceneBegin(m_targetBottom->getRenderTarget());
+
+                for(const auto &entry : m_drawStackBottom2d) { // for every layer
+                    for(const auto &drawable : entry.second) { // draw every object
+                        drawable->draw(m3d::RenderContext(
+                                m_projectionUniform,   // projectionUniform
+                                m_modelViewUniform,    // modelViewUniform
+                                m_lightVecUniform,     // lightVecUniform
+                                m_lightHalfVecUniform, // lightHalfVecUniform
+                                m_lightColorUniform,   // lightColorUniform
+                                m_materialUniform,     // materialUniform
+                                m_3dEnabled, // 3dEnabled
+                                m3d::RenderContext::Mode::Flat,           // mode
+                                m3d::RenderContext::Stereo3dSide::Left,   // side
+                                m3d::RenderContext::ScreenTarget::Bottom, // target
+                                m_projection, // projection
+                                m_modelView   // modelview
+                            ));
+                    }
+                }
+
+                m_drawStackBottom2d.clear();
+            }
+
+            if(m_drawStackTop2d.size() > 0) {
                 C2D_SceneBegin(m_targetTopLeft->getRenderTarget());
 
-                for(const auto &entry : m_drawStackTop) { // for every layer
+                for(const auto &entry : m_drawStackTop2d) { // for every layer
                     for(const auto &drawable : entry.second) { // draw every object
-                        drawable->draw(m_3dEnabled, m3d::Screen::Stereo3dSide::Left);
+                        drawable->draw(m3d::RenderContext(
+                                m_projectionUniform,   // projectionUniform
+                                m_modelViewUniform,    // modelViewUniform
+                                m_lightVecUniform,     // lightVecUniform
+                                m_lightHalfVecUniform, // lightHalfVecUniform
+                                m_lightColorUniform,   // lightColorUniform
+                                m_materialUniform,     // materialUniform
+                                m_3dEnabled, // 3dEnabled
+                                m3d::RenderContext::Mode::Flat,         // mode
+                                m3d::RenderContext::Stereo3dSide::Left, // side
+                                m3d::RenderContext::ScreenTarget::Top,  // target
+                                m_projection, // projection
+                                m_modelView   // model
+                            ));
                     }
                 }
 
                 if(m_3dEnabled) {
                     C2D_SceneBegin(m_targetTopRight->getRenderTarget());
 
-                    for(const auto &entry : m_drawStackTop) { // for every layer
+                    for(const auto &entry : m_drawStackTop2d) { // for every layer
                         for(const auto &drawable : entry.second) { // draw every object
-                            drawable->draw(m_3dEnabled, m3d::Screen::Stereo3dSide::Right);
+                            drawable->draw(m3d::RenderContext(
+                                    m_projectionUniform,   // projectionUniform
+                                    m_modelViewUniform,    // modelViewUniform
+                                    m_lightVecUniform,     // lightVecUniform
+                                    m_lightHalfVecUniform, // lightHalfVecUniform
+                                    m_lightColorUniform,   // lightColorUniform
+                                    m_materialUniform,     // materialUniform
+                                    m_3dEnabled, // 3dEnabled
+                                    m3d::RenderContext::Mode::Flat,          // mode
+                                    m3d::RenderContext::Stereo3dSide::Right, // side
+                                    m3d::RenderContext::ScreenTarget::Top,   // target
+                                    m_projection, // projection
+                                    m_modelView   // model
+                                ));
                         }
                     }
                 }
-            }
 
-            C3D_FrameEnd(0);
-            m_drawStackTop.clear();
-            m_drawStackBottom.clear();
+                m_drawStackTop2d.clear();
+            }
         }
+
+        C3D_FrameEnd(0);
     }
 
-    int Screen::getScreenWidth(m3d::Screen::ScreenTarget t_target) {
-        return t_target == m3d::Screen::ScreenTarget::Top ? 400 : 320;
+    int Screen::getScreenWidth(m3d::RenderContext::ScreenTarget t_target) {
+        return t_target == m3d::RenderContext::ScreenTarget::Top ? 400 : 320;
     }
 
     int Screen::getScreenHeight() {
         return 240;
     }
 
-    // private methods
     void Screen::clear() {
         C2D_TargetClear(m_targetTopLeft->getRenderTarget(), m_clearColorTop.getRgba8());
         C2D_TargetClear(m_targetTopRight->getRenderTarget(), m_clearColorTop.getRgba8());
         C2D_TargetClear(m_targetBottom->getRenderTarget(), m_clearColorBottom.getRgba8());
+    }
+
+    // private methods
+    void Screen::prepare() {
+        C3D_BindProgram(&m_shader);
+
+        // initialize and configure attributes
+        m_attributeInfo = C3D_GetAttrInfo();
+        AttrInfo_Init(m_attributeInfo);
+        AttrInfo_AddLoader(m_attributeInfo, 0, GPU_FLOAT, 3); // v0=position
+        AttrInfo_AddLoader(m_attributeInfo, 1, GPU_FLOAT, 2); // v1=texcoord
+        AttrInfo_AddLoader(m_attributeInfo, 2, GPU_FLOAT, 3); // v2=normal
+
+        // Configure the first fragment shading substage to blend the fragment primary color
+        // with the fragment secondary color.
+        // See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
+        // C3D_TexEnv* environment = C3D_GetTexEnv(0);
+        // C3D_TexEnvSrc(environment, C3D_Both, GPU_FRAGMENT_PRIMARY_COLOR, GPU_FRAGMENT_SECONDARY_COLOR, (GPU_TEVSRC) 0);
+        // C3D_TexEnvOp(environment, C3D_Both, 0, 0, 0);
+        // C3D_TexEnvFunc(environment, C3D_Both, GPU_ADD);
+        C3D_TexEnv* env = C3D_GetTexEnv(0);
+        C3D_TexEnvInit(env);
+        C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, GPU_PRIMARY_COLOR);
+        C3D_TexEnvFunc(env, C3D_Both, GPU_MODULATE);
     }
 } /* m3d */
