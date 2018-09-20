9ecbfa68f214adcc02fb3642b931a103ef378869
Liam Minopulos <stunthacks@gmail.com>
2018-09-20 16:07:01 +0200

    Add graphics-private

---
+++ m3dialib/includes/m3d/private/graphics.hpp
@@ -0,0 +1,17 @@
+#ifndef GRAPHICS_PRIVATE_H
+#define GRAPHICS_PRIVATE_H
+
+#pragma once
+#include <citro3d.h>
+
+namespace m3d {
+    namespace priv {
+        namespace graphics {
+            extern const C3D_Material material;
+            extern const C3D_Mtx materialMatrix;
+        } /* graphics */
+    } /* priv */
+} /* m3d */
+
+
+#endif /* end of include guard: GRAPHICS_PRIVATE_H */
