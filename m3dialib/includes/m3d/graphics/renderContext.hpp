59984702014b0fd39630b4adcad45405ba95aecb
Liam Minopulos <stunthacks@gmail.com>
2018-09-20 16:09:36 +0200

    Add RenderContext-class

---
+++ m3dialib/includes/m3d/graphics/renderContext.hpp
@@ -0,0 +1,84 @@
+#ifndef RENDERCONTEXT_H
+#define RENDERCONTEXT_H
+
+#pragma once
+#include <citro3d.h>
+
+namespace m3d {
+    class RenderContext {
+    public:
+        /**
+         * @brief Defines the screen targets (top/bottom)
+         */
+        enum class ScreenTarget {
+            Top,     ///< Top screen
+            Bottom   ///< Bottom/touch screen
+        };
+
+        /**
+         * @brief Defines the two sides in stereoscopic 3D mode
+         */
+        enum class Stereo3dSide {
+            Left, ///< Left side
+            Right ///< Right side
+        };
+
+        /**
+         * @brief Defines the two drawing-modes
+         */
+        enum class Mode {
+            Flat,   ///< 2D
+            Spatial ///< 3D
+        };
+
+        RenderContext(
+            int t_projectionUniform,
+            int t_modelViewUniform,
+            int t_lightVecUniform,
+            int t_lightHalfVecUniform,
+            int t_lightColorUniform,
+            int t_materialUniform,
+            bool t_3dEnabled,
+            m3d::RenderContext::Mode t_mode,
+            m3d::RenderContext::Stereo3dSide t_side,
+            m3d::RenderContext::ScreenTarget t_target,
+            C3D_Mtx& t_projection,
+            C3D_Mtx& t_modelView
+        );
+
+        int getProjectionUniform();
+
+        int getModelViewUniform();
+
+        int getLightVecUniform();
+
+        int getLightHalfVecUniform();
+
+        int getLightColorUniform();
+
+        int getMaterialUniform();
+
+        bool is3dEnabled();
+
+        m3d::RenderContext::Mode getMode();
+
+        m3d::RenderContext::Stereo3dSide getSide();
+
+        m3d::RenderContext::ScreenTarget getScreenTarget();
+
+        C3D_Mtx& getProjectionMatrix();
+
+        C3D_Mtx& getModelViewMatrix();
+
+    private:
+        /* data */
+        int m_projectionUniformLocation, m_modelViewUniformLocation, m_lightVecUniformLocation, m_lightHalfVecUniformLocation, m_lightColorUniformLocation, m_materialUniformLocation;
+        bool m_3dEnabled;
+        m3d::RenderContext::Mode m_mode;
+        m3d::RenderContext::Stereo3dSide m_side;
+        m3d::RenderContext::ScreenTarget m_target;
+        C3D_Mtx &m_projection, &m_modelView;
+    };
+} /* m3d */
+
+#endif /* end of include guard: RENDERCONTEXT_H */
---
+++ m3dialib/source/graphics/renderContext.cpp
@@ -0,0 +1,78 @@
+#include "m3d/graphics/renderContext.hpp"
+
+namespace m3d {
+    RenderContext::RenderContext(
+        int t_projectionUniform,
+        int t_modelViewUniform,
+        int t_lightVecUniform,
+        int t_lightHalfVecUniform,
+        int t_lightColorUniform,
+        int t_materialUniform,
+        bool t_3dEnabled,
+        m3d::RenderContext::Mode t_mode,
+        m3d::RenderContext::Stereo3dSide t_side,
+        m3d::RenderContext::ScreenTarget t_target,
+        C3D_Mtx& t_projection,
+        C3D_Mtx& t_modelView
+    ) :
+    m_projectionUniformLocation(t_projectionUniform),
+    m_modelViewUniformLocation(t_modelViewUniform),
+    m_lightVecUniformLocation(t_lightVecUniform),
+    m_lightHalfVecUniformLocation(t_lightHalfVecUniform),
+    m_lightColorUniformLocation(t_lightColorUniform),
+    m_materialUniformLocation(t_materialUniform),
+    m_3dEnabled(t_3dEnabled),
+    m_mode(t_mode),
+    m_side(t_side),
+    m_target(t_target),
+    m_projection(t_projection),
+    m_modelView(t_modelView) { /* do nothing */ }
+
+    int RenderContext::getProjectionUniform() {
+        return m_projectionUniformLocation;
+    }
+
+    int RenderContext::getModelViewUniform() {
+        return m_modelViewUniformLocation;
+    }
+
+    int RenderContext::getLightVecUniform() {
+        return m_lightVecUniformLocation;
+    }
+
+    int RenderContext::getLightHalfVecUniform() {
+        return m_lightHalfVecUniformLocation;
+    }
+
+    int RenderContext::getLightColorUniform() {
+        return m_lightColorUniformLocation;
+    }
+
+    int RenderContext::getMaterialUniform() {
+        return m_materialUniformLocation;
+    }
+
+    bool RenderContext::is3dEnabled() {
+        return m_3dEnabled;
+    }
+
+    m3d::RenderContext::Mode RenderContext::getMode() {
+        return m_mode;
+    }
+
+    m3d::RenderContext::Stereo3dSide RenderContext::getSide() {
+        return m_side;
+    }
+
+    m3d::RenderContext::ScreenTarget RenderContext::getScreenTarget() {
+        return m_target;
+    }
+
+    C3D_Mtx& RenderContext::getProjectionMatrix() {
+        return m_projection;
+    }
+
+    C3D_Mtx& RenderContext::getModelViewMatrix() {
+        return m_modelView;
+    }
+} /* m3d */
