e132d88947ad71ae66f189bd4964680fb5f569c9
Liam Minopulos <stunthacks@gmail.com>
2018-09-23 20:04:36 +0200

    Update RenderContext-class

--- m3dialib/source/graphics/renderContext.cpp
+++ m3dialib/source/graphics/renderContext.cpp
@@ -2,36 +2,28 @@
 
 namespace m3d {
     RenderContext::RenderContext(
-        int t_projectionUniform,
-        int t_modelViewUniform,
+        int t_modelUniform,
         bool t_3dEnabled,
         m3d::RenderContext::Mode t_mode,
         m3d::RenderContext::Stereo3dSide t_side,
         m3d::RenderContext::ScreenTarget t_target,
-        C3D_Mtx& t_projection,
-        C3D_Mtx& t_modelView,
+        C3D_Mtx& t_model,
         C3D_LightEnv& t_lightEnv,
         C3D_Light& t_light,
         C3D_LightLut& t_lightLut
     ) :
-    m_projectionUniformLocation(t_projectionUniform),
-    m_modelViewUniformLocation(t_modelViewUniform),
+    m_modelUniformLocation(t_modelUniform),
     m_3dEnabled(t_3dEnabled),
     m_mode(t_mode),
     m_side(t_side),
     m_target(t_target),
-    m_projection(t_projection),
-    m_modelView(t_modelView),
+    m_model(t_model),
     m_lightEnv(t_lightEnv),
     m_light(t_light),
     m_lightLut(t_lightLut) { /* do nothing */ }
 
-    int RenderContext::getProjectionUniform() {
-        return m_projectionUniformLocation;
-    }
-
-    int RenderContext::getModelViewUniform() {
-        return m_modelViewUniformLocation;
+    int RenderContext::getModelUniform() {
+        return m_modelUniformLocation;
     }
 
     bool RenderContext::is3dEnabled() {
@@ -50,12 +42,8 @@ namespace m3d {
         return m_target;
     }
 
-    C3D_Mtx& RenderContext::getProjectionMatrix() {
-        return m_projection;
-    }
-
-    C3D_Mtx& RenderContext::getModelViewMatrix() {
-        return m_modelView;
+    C3D_Mtx& RenderContext::getModelMatrix() {
+        return m_model;
     }
 
     C3D_LightEnv& RenderContext::getLightEnvironment() {
