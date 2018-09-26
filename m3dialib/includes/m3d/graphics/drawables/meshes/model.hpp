500c67fe5ad116a64fa77168739af51a3f2b2bdc
Liam Minopulos <stunthacks@gmail.com>
2018-09-26 11:06:49 +0200

    Add documentation

--- m3dialib/includes/m3d/graphics/drawables/meshes/model.hpp
+++ m3dialib/includes/m3d/graphics/drawables/meshes/model.hpp
@@ -14,6 +14,12 @@ namespace m3d {
      */
     class Model: public m3d::Mesh {
     public:
+        /**
+         * @brief Loads a model from a file (currently .obj-only)
+         * @param  t_filename The path to the file
+         * @return            `true` if loading was successful, `false` otherwise
+         * @warning Keep in mind, that your model needs to be triangulated in order to load correctly. An option to automatically triangulate will be added in the future however it will still be faster to triangulate them before-hand.
+         */
         bool loadFromFile(const std::string& t_filename);
     };
 } /* m3d */
