674c67ba7e6d6e745ff397b12805219a00287a43
Liam Minopulos <stunthacks@gmail.com>
2018-09-26 11:32:55 +0200

    Add running indicator

--- m3dialib/source/core/thread.cpp
+++ m3dialib/source/core/thread.cpp
@@ -2,24 +2,26 @@
 
 namespace m3d {
     Thread::Thread() :
-                m_running(false),
-                m_started(false) { /* do nothing */ }
+                m_started(false),
+                m_running(false) { /* do nothing */ }
 
     Thread::Thread(std::function<void(m3d::Parameter)> t_function, m3d::Parameter t_parameter, bool t_autostart, bool t_detached, unsigned long long int t_stackSize) :
-                m_running(false),
-                m_started(false) {
+                m_started(false),
+                m_running(false) {
         initialize(t_function, t_parameter, t_autostart, t_detached, t_stackSize);
     }
 
     Thread::~Thread() {
-        threadJoin(m_thread, U64_MAX);
-        threadFree(m_thread);
+        join();
+
+        if (m_started) threadFree(m_thread);
     }
 
     void Thread::initialize(std::function<void(m3d::Parameter)> t_function, m3d::Parameter t_parameter, bool t_autostart, bool t_detached, unsigned long long int t_stackSize) {
         m_stackSize = t_stackSize;
         m_data.m_parameter = t_parameter;
         m_data.m_function = t_function;
+        m_data.m_running = &m_running;
 
         if (t_autostart) {
             start(t_detached);
@@ -31,12 +33,12 @@ namespace m3d {
     }
 
     void Thread::start(bool t_detached) {
-        if (!m_started && !m_running) {
-            s32 prio;
-            svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
-            m_thread = threadCreate(threadFunction, &m_data, m_stackSize, prio - 1, -2, t_detached);
+        if (!m_running) {
             m_started = true;
             m_running = true;
+            s32 prio;
+            svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
+            m_thread = threadCreate(threadFunction, &m_data, m_stackSize, prio + 1, -2, t_detached);
         }
     }
 
@@ -47,7 +49,7 @@ namespace m3d {
     }
 
     void Thread::join(long long unsigned int t_timeout) {
-        if (m_started) {
+        if (m_running) {
             threadJoin(m_thread, t_timeout);
             threadFree(m_thread);
             m_running = false;
@@ -55,17 +57,22 @@ namespace m3d {
         }
     }
 
-   void Thread::sleep() {
-       svcSleepThread(0);
-   }
+    bool Thread::isRunning() {
+        return m_running;
+    }
 
-   void Thread::sleep(int t_milliseconds) {
-       svcSleepThread(1000000 * t_milliseconds);
-   }
+    void Thread::sleep() {
+        svcSleepThread(0);
+    }
 
-   // private methods
-   void Thread::threadFunction(void* arg) {
-       m3d::Thread::ThreadData data = *static_cast<m3d::Thread::ThreadData*>(arg);
-       data.m_function(data.m_parameter);
-   }
+    void Thread::sleep(int t_milliseconds) {
+        svcSleepThread(1000000 * t_milliseconds);
+    }
+
+    // private methods
+    void Thread::threadFunction(void* arg) {
+        m3d::Thread::ThreadData data = *static_cast<m3d::Thread::ThreadData*>(arg);
+        data.m_function(data.m_parameter);
+        *data.m_running = false;
+    }
 } /* m3d */
