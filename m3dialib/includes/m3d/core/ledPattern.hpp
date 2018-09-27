923e0745a3e6e72b09d7aead65c2f83e003a5925
Liam Minopulos <stunthacks@gmail.com>
2018-09-27 08:36:44 +0200

    Add documentation

--- m3dialib/includes/m3d/core/ledPattern.hpp
+++ m3dialib/includes/m3d/core/ledPattern.hpp
@@ -21,28 +21,75 @@ namespace m3d {
      */
     class LEDPattern {
     public:
+        /**
+         * @brief Constructs the LED pattern
+         */
         LEDPattern();
 
+        /**
+         * @brief Initializes the LED pattern with a constant color
+         * @param t_color The color
+         */
         LEDPattern(m3d::Color t_color);
 
+        /**
+         * @brief Adds a keyframe to the pattern
+         * @param t_sample The sample-position of the keyframe
+         * @param t_color  The color of the keyframe
+         * @param t_hold   Whether to hold the value of the last keyframe until this one or not
+         */
         void addKeyFrame(int t_sample, m3d::Color t_color, bool t_hold = false);
 
+        /**
+         * @brief Deletes the keyframe at the given sample-position
+         * @param t_sample The sample-position
+         */
         void deleteKeyframe(int t_sample);
 
+        /**
+         * @brief Clears all keyframes
+         */
         void clearKeyframes();
 
+        /**
+         * @brief Sets the time the pattern should play until it starts again
+         * @param t_time The time in seconds (ranging from 0.25 to 16.0)
+         */
         void setTime(float t_time);
 
+        /**
+         * @brief Returns the time the pattern plays until it starts again
+         * @return The time in seconds
+         */
         float getTime();
 
+        /**
+         * @brief Sets the time the pattern should wait before it starts again
+         * @param t_time The time in seconds (ranging from 0 to 16)
+         * @note When a time >16 is set, the pattern won't loop but will instead keep displaying the last color until it is either stopped or a different pattern is played
+         */
         void setDelay(float t_delay);
 
+        /**
+         * @brief Returns the time the pattern waits before it starts again
+         * @return The time in seconds
+         */
         float getDelay();
 
+        /**
+         * @brief Plays the pattern
+         */
         void play();
 
+        /**
+         * @brief Returns whether a pattern is currently playing or not
+         * @return `true` if there is an LED pattern playing, `false` otherwise
+         */
         static bool isPlaying();
 
+        /**
+         * @brief Stops any LED pattern that's currently active
+         */
         static void stop();
 
     private:
