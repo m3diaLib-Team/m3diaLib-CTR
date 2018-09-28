58ccfde8a453d2ae222b8ee8e56384c85e1743e2
Liam Minopulos <stunthacks@gmail.com>
2018-09-28 14:34:37 +0200

    Reset texture environments

--- m3dialib/source/graphics/screen.cpp
+++ m3dialib/source/graphics/screen.cpp
@@ -444,6 +444,11 @@ namespace m3d {
         C3D_TexEnvSrc(env, C3D_Both, GPU_FRAGMENT_PRIMARY_COLOR, GPU_FRAGMENT_SECONDARY_COLOR, GPU_PRIMARY_COLOR);
         C3D_TexEnvFunc(env, C3D_Both, GPU_ADD);
 
+        C3D_TexEnvInit(C3D_GetTexEnv(2));
+        C3D_TexEnvInit(C3D_GetTexEnv(3));
+        C3D_TexEnvInit(C3D_GetTexEnv(4));
+        C3D_TexEnvInit(C3D_GetTexEnv(5));
+
         C3D_CullFace(m_useCulling ? GPU_CULL_BACK_CCW : GPU_CULL_NONE);
     }
 
