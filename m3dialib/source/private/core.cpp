d936a4785b17f6bd74c90b9a521e8e12da0fa16e
Liam Minopulos <stunthacks@gmail.com>
2018-10-04 12:19:09 +0200

    Add soc-buffer

--- m3dialib/source/private/core.cpp
+++ m3dialib/source/private/core.cpp
@@ -3,8 +3,10 @@
 namespace m3d {
     namespace priv {
         namespace core {
+            bool socuInitialized = false;
             std::atomic<bool> ledPatternRunning(false);
             Handle ptmsysmHandle = 0;
+            u32* socubuf = nullptr;
         } /* core */
     } /* priv */
 } /* m3d */
