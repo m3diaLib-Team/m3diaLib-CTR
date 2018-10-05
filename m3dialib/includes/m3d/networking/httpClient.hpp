c5765317dc817ab0bfea1ceab8b3ef9b9452ce85
Liam Minopulos <stunthacks@gmail.com>
2018-10-05 15:12:27 +0200

    Add documentation

--- m3dialib/includes/m3d/networking/httpClient.hpp
+++ m3dialib/includes/m3d/networking/httpClient.hpp
@@ -17,64 +17,150 @@ namespace m3d {
      */
     class HTTPClient {
     public:
+        /**
+         * @brief Represents the state of the client
+         */
         enum class Status {
-            NotStarted,
-            Running,
-            OK,
-            Aborted,
-            Failed,
-            ResolveFailed,
-            AccessDenied,
-            InternalError
+            NotStarted,    ///< No request was made yet
+            Running,       ///< A asynchronous request is running
+            OK,            ///< The last request was successfull
+            Aborted,       ///< The last request was aborted by the user
+            Failed,        ///< The last request failed
+            ResolveFailed, ///< The last request failed to resolve the host
+            AccessDenied,  ///< The last request failed because the access to the ressource was denied
+            InternalError  ///< The last request failed due to an internal error
         };
 
+        /**
+         * @brief Default constructor
+         */
         HTTPClient();
 
+        /**
+         * @brief Destructor
+         */
         ~HTTPClient();
 
         /** @name Performing requests */
         ///@{
+        /**
+         * @brief Starts a get request
+         * @param  t_url The url to request
+         * @return       The response body
+         */
         const std::string get(const std::string& t_url);
 
+        /**
+         * @brief Starts an asynchronous get request
+         * @param t_url The url to request
+         */
         void getAsync(const std::string& t_url);
 
+        /**
+         * @brief Starts a get request and saves the response to a file
+         * @param t_url      The url to request
+         * @param t_filename The file to save to
+         */
         void getToFile(const std::string& t_url, const std::string& t_filename);
 
+        /**
+         * @brief Starts a asynchronous get request and saves the response to a file
+         * @param t_url      The url to request
+         * @param t_filename The file to save to
+         */
         void getToFileAsync(const std::string& t_url, const std::string& t_filename);
 
+        /**
+         * @brief Starts a post request
+         * @param  t_url        The url to request
+         * @param  t_parameters The post-parameters
+         * @return              The response body
+         */
         const std::string post(const std::string& t_url, const std::string& t_parameters);
 
+        /**
+         * @brief Starts a post request and saves the response to a file
+         * @param t_url        The url to request
+         * @param t_parameters The post-parameters
+         * @param t_filename   The file to save to
+         */
         void postAsync(const std::string& t_url, const std::string& t_parameters);
 
+        /**
+         * @brief Starts a post request and saves the response to a file
+         * @param t_url        The url to request
+         * @param t_parameters The post-parameters
+         * @param t_filename   The file to save to
+         */
         void postToFile(const std::string& t_url, const std::string& t_parameters, const std::string& t_filename);
 
+        /**
+         * @brief Starts a asynchronous post request and saves the response to a file
+         * @param t_url        The url to request
+         * @param t_parameters The post-parameters
+         * @param t_filename   The file to save to
+         */
         void postToFileAsync(const std::string& t_url, const std::string& t_parameters, const std::string& t_filename);
         ///@}
 
         /** @name Control methods */
         ///@{
+        /**
+         * @brief Waits for an asynchronous call to finish
+         */
         void waitForFinish();
 
+        /**
+         * @brief Returns whether the last request is finished or not
+         * @return `true` if it is finished, `false` if it is still running
+         */
         bool isFinished();
 
+        /**
+         * @brief Cancels the current request
+         */
         void cancel();
         ///@}
 
         /** @name Response handling */
         ///@{
+        /**
+         * @brief Returns the current state of the client
+         * @return The current state
+         */
         m3d::HTTPClient::Status getStatus();
 
+        /**
+         * @brief Returns the HTTP status code of the last request
+         * @return The HTTP status code
+         */
         int getStatusCode();
 
+        /**
+         * @brief Returns the response body of the last request
+         * @return The response
+         */
         const std::string getResponse();
         ///@}
 
         /** @name Progress */
         ///@{
+        /**
+         * @brief Returns the progress of the currently running request
+         * @return The progress, ranging from 0.0 to 1.0
+         */
         double getProgress();
 
+        /**
+         * @brief Returns the progress in bytes of the currently running request
+         * @return The progress in bytes
+         */
         double getProgressSize();
 
+        /**
+         * @brief Returns the total size of the response of the currently running request
+         * @return The total size in bytes
+         */
         double getTotalSize();
         ///@}
 
