bbb8476b59a568e3efafd55733f34fb0c1456609
Liam Minopulos <stunthacks@gmail.com>
2018-10-05 13:57:53 +0200

    Add networking include

---
+++ m3dialib/includes/m3d/networking/networking.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file networking.hpp
+ * @brief Includes all files related to networking
+ */
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
+#pragma once
+
+#include "httpClient.hpp"
+
+
+#endif /* end of include guard: NETWORKING_H */
