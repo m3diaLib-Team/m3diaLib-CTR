dad95101f1c69f591025cf249514ede26a30c1fe
Liam Minopulos <stunthacks@gmail.com>
2018-10-16 18:00:31 +0200

    Add new vertex struct

--- m3dialib/includes/m3d/private/graphics.hpp
+++ m3dialib/includes/m3d/private/graphics.hpp
@@ -2,12 +2,24 @@
 #define GRAPHICS_PRIVATE_H
 
 #pragma once
-#include "m3d/graphics/camera.hpp"
+#include <3ds.h>
 
 namespace m3d {
+    class Camera;
+
     namespace priv {
         namespace graphics {
+            struct Vertex {
+                float pos[3];
+                float texcoord[2];
+                float normals[3];
+                float blend[2];
+                u32 color;
+            };
+
             extern m3d::Camera defaultCamera0, defaultCamera1;
+
+            extern Vertex *vertexBuffer2d, *vertexBuffer3d;
         } /* graphics */
     } /* priv */
 } /* m3d */
--- m3dialib/source/private/graphics.cpp
+++ m3dialib/source/private/graphics.cpp
@@ -1,9 +1,12 @@
+#include "m3d/graphics/camera.hpp"
 #include "m3d/private/graphics.hpp"
 
 namespace m3d {
     namespace priv {
         namespace graphics {
             m3d::Camera defaultCamera0, defaultCamera1;
+
+            Vertex *vertexBuffer2d = nullptr, *vertexBuffer3d = nullptr;
         } /* graphics */
     } /* priv */
 } /* m3d */
